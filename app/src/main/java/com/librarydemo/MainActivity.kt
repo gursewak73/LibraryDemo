41716037afd4d6efd9d16431c1981c141143e5e5
gursewaksingh <gursewak73@gmail.com>
2018-03-04 15:56:17 +0530

    library sdk

---
+++ app/src/main/java/com/librarydemo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.librarydemo
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mylibrary.TestLibrary
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        var testLibrary = TestLibrary()
+//        testLibrary.start()
+//        testLibrary.stop()
+    }
+}
---
+++ mylibrary/src/main/java/com/mylibrary/TestLibrary.java
@@ -0,0 +1,16 @@
+package com.mylibrary;
+
+/**
+ * Created by gursewaksingh on 02/03/18.
+ */
+
+public class TestLibrary {
+
+    public void start() {
+
+    }
+
+    public void stop(){
+
+    }
+}
